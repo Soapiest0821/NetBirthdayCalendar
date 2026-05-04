fb9217633c37befb7464d4fe206aa01a4403d797
사피 <soapiest0821@gmail.com>
2026-05-04 16:04:49 +0900

    react 세팅, 기초 프론트

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="title">
+        <h1>제목</h1>
+      </div>
+
+      <div className="content">내용</div>
+    </div>
+  );
+}
+
+export default App;
