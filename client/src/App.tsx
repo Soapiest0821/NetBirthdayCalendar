6bce2fc689ee78aea6ba6f28b31fb55e1be83108
사피 <soapiest0821@gmail.com>
2026-05-04 23:48:08 +0900

    끝!!!!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,17 @@ function App() {
   return (
     <div className="wrapper">
       <div className="title">
-        <h1>제목</h1>
+        <h1>{new Date().getMonth() + 1 + "월"}</h1>
       </div>
 
-      <div className="content">내용</div>
+      <div className="content" id="calendar">
+        {Array.from({ length: 42 }).map((_, i) => (
+          <div key={i} className="cell"></div>
+        ))}
+      </div>
+
+      <div className="datelist" id="upcoming"></div>
+      <div className="datelist" id="gone"></div>
     </div>
   );
 }
